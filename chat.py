9a0988e1bdc593ebb57caa34b3e408996aed1725
szymon <szymon.pardyka@gmail.com>
2023-05-23 23:05:04 +0200

    Even more data in intents.json (New data included in trained model)

--- chat.py
+++ chat.py
@@ -1,11 +1,10 @@
-import random
 import json
 import pickle
-import numpy as np
+import random
 
 import nltk
+import numpy as np
 from nltk.stem import WordNetLemmatizer
-
 from tensorflow.keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
@@ -15,10 +14,12 @@ words = pickle.load(open('words.pickle', 'rb'))
 classes = pickle.load(open('classes.pickle', 'rb'))
 model = load_model('chatbot_model.h5')
 
+
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
     return sentence_words
+
 
 def bag_of_words(sentence):
     sentence_words = clean_up_sentence(sentence)
@@ -29,17 +30,19 @@ def bag_of_words(sentence):
                 bag[i] = 1
     return np.array(bag)
 
+
 def predict_class(sentence):
     bow = bag_of_words(sentence)
     res = model.predict(np.array([bow]))[0]
-    ERROR_TRESHOLD = 0.25
-    results = [[i,r] for i,r in enumerate(res) if r > ERROR_TRESHOLD]
+    error_threshold = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > error_threshold]
 
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
         return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
     return return_list
+
 
 def get_response(intents_list, intents_json):
     tag = intents_list[0]['intent']
@@ -49,6 +52,7 @@ def get_response(intents_list, intents_json):
             result = random.choice(i['responses'])
             break
     return result
+
 
 while True:
     message = input("")

26d7b725c53cbef49ad4aba2f13bf15f8f2390a6
Szymon Pardyka <szymon.pardyka@gmail.com>
2023-05-24 13:44:45 +0000

    models input size fix

--- train.py
+++ train.py
@@ -69,4 +69,4 @@ optimizer = Adam(learning_rate=lr_schedule)
 model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save('chatbot_internal_model.h5', hist)
+model.save('chatbot_model.h5', hist)

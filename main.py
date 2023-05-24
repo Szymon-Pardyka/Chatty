8a73791e5e5ab66437f3e7f22d55f4dd4de775e9
Rostyslav Kholodnytskyi <rostyslav.kholodnytskyi@gmail.com>
2023-05-24 11:21:57 +0000

    add rest

---
+++ main.py
@@ -0,0 +1,76 @@
+import json
+import pickle
+import random
+
+import nltk
+import numpy as np
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+import json
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pickle', 'rb'))
+classes = pickle.load(open('classes.pickle', 'rb'))
+model = load_model('chatbot_model.h5')
+
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    error_threshold = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > error_threshold]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+
+@app.route('/', methods=['GET'])
+def query_records():
+    return "xyi"
+
+
+@app.route('/', methods=['POST'])
+def update_record():
+    record = json.loads(request.data)
+    print(record["question"])
+    ints = predict_class(record["question"])
+    res = get_response(ints, intents)
+    return jsonify(res)
+
+
+app.run(debug=True)
+
+
